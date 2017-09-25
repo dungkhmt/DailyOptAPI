4c9084f6f943ab801d442a2737a9ce4e0cac674d
dungkhmt <dungkhmt@gmail.com>
2017-09-25 21:48:30 +0700

    initialization

---
+++ dailyoptapi/src/main/java/test/TestInput.java
@@ -0,0 +1,28 @@
+package test;
+
+public class TestInput {
+	private int a;
+	private int b;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public TestInput(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	public TestInput() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ dailyoptapi/src/main/java/test/TestSolution.java
@@ -0,0 +1,24 @@
+package test;
+
+public class TestSolution {
+	private int sum;
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+
+	public TestSolution(int sum) {
+		super();
+		this.sum = sum;
+	}
+
+	public TestSolution() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
