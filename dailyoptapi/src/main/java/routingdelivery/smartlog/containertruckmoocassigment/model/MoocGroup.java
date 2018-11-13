313054e6cb32f50acf5669eaf6d1074f258ed8a6
sonnv <sonnv188@gmail.com>
2018-11-13 11:09:24 +0700

    add weight and type cont(mooc) constraints
    add some constraints and fix bug

---
+++ dailyoptapi/src/main/java/routingdelivery/smartlog/containertruckmoocassigment/model/MoocGroup.java
@@ -0,0 +1,27 @@
+package routingdelivery.smartlog.containertruckmoocassigment.model;
+
+public class MoocGroup {
+	private String code;
+	private MoocPacking[] packing;
+	
+	public MoocGroup(String code, MoocPacking[] packing){
+		this.code = code;
+		this.packing = packing;
+	}
+	
+	public String getCode(){
+		return this.code;
+	}
+	
+	public void setCode(String code){
+		this.code = code;
+	}
+	
+	public MoocPacking[] getPacking(){
+		return this.packing;
+	}
+	
+	public void setPacking(MoocPacking[] packing){
+		this.packing = packing;
+	}
+}
